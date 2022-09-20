7eeab634a4a0ef4ce38dcf4aeb4081509e6237c8
lukastan93 <lukastan93@gmail.com>
2022-09-20 16:31:31 +0400

    cors fix

--- script.js
+++ script.js
@@ -216,7 +216,7 @@ const api_url ="https://zenquotes.io/api/quotes/";
 
 async function getapi(url)
 {
-  const response = await fetch(url, {mode: 'cors'});
+  const response = await fetch(url, {credentials: 'include'});
   var data = await response.json();
   console.log(data);
 }
